8e03b9312b801c8e837772d48cdefae68c602247
anantbhartiya <anant.bhartiya2@gmail.com>
2023-06-22 01:39:27 +0530

    added custom themes

---
+++ lib/utils/theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Color greentheme = const Color(0xffe9f9dc);
+Color yellowtheme = const Color(0xfffcedca);
+Color pinktheme = const Color(0xfffdd6ce);
+Color bluetheme = const Color(0xffdcedf9);
+Color blacktheme = const Color(0xff262d39);
+
+TextStyle lightTextStyle = GoogleFonts.redRose().copyWith(fontWeight: FontWeight.w300);
+TextStyle regularTextStyle = GoogleFonts.redRose().copyWith(fontWeight: FontWeight.w400);
+TextStyle semiboldTextStyle = GoogleFonts.redRose().copyWith(fontWeight: FontWeight.w600);
+TextStyle boldTextStyle = GoogleFonts.redRose().copyWith(fontWeight: FontWeight.w700);
