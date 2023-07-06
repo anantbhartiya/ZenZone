1205fc3db23d40af45ebe7ebdfb3071f093b9386
anantbhartiya <anant.bhartiya2@gmail.com>
2023-07-06 19:36:00 +0530

    made minor changes to text style

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -7,7 +7,7 @@ Color pinktheme = const Color(0xfffdd6ce);
 Color bluetheme = const Color(0xffdcedf9);
 Color blacktheme = const Color(0xff262d39);
 
-TextStyle lightTextStyle = GoogleFonts.redRose().copyWith(fontWeight: FontWeight.w300);
-TextStyle regularTextStyle = GoogleFonts.redRose().copyWith(fontWeight: FontWeight.w400);
-TextStyle semiboldTextStyle = GoogleFonts.redRose().copyWith(fontWeight: FontWeight.w600);
-TextStyle boldTextStyle = GoogleFonts.redRose().copyWith(fontWeight: FontWeight.w700);
+TextStyle lightTextStyle = GoogleFonts.redRose().copyWith(fontWeight: FontWeight.w300, color: blacktheme);
+TextStyle regularTextStyle = GoogleFonts.redRose().copyWith(fontWeight: FontWeight.w400, color: blacktheme);
+TextStyle semiboldTextStyle = GoogleFonts.redRose().copyWith(fontWeight: FontWeight.w600, color: blacktheme);
+TextStyle boldTextStyle = GoogleFonts.redRose().copyWith(fontWeight: FontWeight.w700, color: blacktheme);
